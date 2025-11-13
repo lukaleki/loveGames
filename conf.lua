e4536e9b4698184902d225deeb1a1cf608b78847
Luka <lekishvililuka76@gmail.com>
2025-11-13 17:35:59 +0400

    score and time

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.window.title = "Lua Game"
+    t.window.resizable = true
+    t.window.minwidth = 600
+    t.window.minheight = 600
+    t.window.fullscreen = false
+    t.window.fullscreentype = "desktop"
+end
--- main.lua
+++ main.lua
@@ -1,11 +1,33 @@
+local score = 0
+
 function love.load()
- 
+    love.graphics.setDefaultFilter("nearest", "nearest")
 end
 
 function love.update(dt)
-
+    score = score + 1
 end
 
 function love.draw()
-    love.graphics.print("Hello World!", 400, 300)
+    time = math.floor(love.timer.getTime())
+    standardPadding = 10
+
+    scoreText = "SCORE: " .. score
+    font = love.graphics.getFont()
+    scoreTextWidth = (font:getWidth(scoreText)) * 2
+
+    timeText = "TIME: " .. time
+    TimeTextWidth = (font:getWidth(timeText)) * 2
+
+    windowWidth = love.graphics.getWidth()
+    windowHeight = love.graphics.getHeight()
+
+    scoreTextX = standardPadding
+    scoreTextY = standardPadding
+
+    timeTextX = windowWidth - TimeTextWidth - standardPadding
+    timeTextY = standardPadding
+
+    love.graphics.print(scoreText, scoreTextX, scoreTextY, 0, 2 )
+    love.graphics.print(timeText , timeTextX, timeTextY, 0, 2)
 end
