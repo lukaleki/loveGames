d0d2e2dacb9c2ca45c3520b0ca078a91908e3b03
spyneti <gchalidze11@gmail.com>
2025-11-13 17:28:09 +0400

    CharacterMovement

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 function love.load()
- 
+    require "mainCharacter"
+    mainCharacter.load()
 end
 
 function love.update(dt)
-
+    mainCharacter.update(dt)
 end
 
 function love.draw()
+    mainCharacter.draw()
     love.graphics.print("Hello World!", 400, 200)
 end
